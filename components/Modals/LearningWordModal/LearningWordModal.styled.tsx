ac795ccc4378b0ba3ce82cbdd6a9105c92bef5b3
FeMaxxx <119453050+FeMaxxx@users.noreply.github.com>
2023-08-09 22:13:06 +0300

    Сreated a modal window with information about the word

---
+++ components/Modals/LearningWordModal/LearningWordModal.styled.tsx
@@ -0,0 +1,144 @@
+import styled from "@emotion/styled";
+import { mediaSizes } from "@/constants";
+import { useSvg } from "@/hooks/useSvg";
+
+const { Cross, VocaryLerningSmile } = useSvg;
+
+export const BackDrop = styled.div`
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  backdrop-filter: blur(8px);
+
+  opacity: 0;
+  pointer-events: none;
+
+  transition: opacity var(--animation);
+
+  &.active {
+    opacity: 1;
+    pointer-events: auto;
+  }
+`;
+
+export const Modal = styled.div`
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+
+  display: flex;
+  align-items: center;
+  padding: 20px;
+
+  width: min(90vw, 1000px);
+
+  border: 2px solid var(--primaryColor);
+  border-radius: 30px;
+  background-color: var(--modalBGColor);
+
+  @media screen and (min-width: ${mediaSizes.mobile}) {
+    border: 3px solid var(--primaryColor);
+    border-radius: 50px;
+    padding: 30px;
+  }
+`;
+
+export const CloseButtonWrap = styled.div`
+  position: absolute;
+  top: -35px;
+  right: -3px;
+`;
+
+export const Vocary = styled(VocaryLerningSmile)`
+  display: none;
+
+  @media screen and (min-width: ${mediaSizes.tablet}) {
+    display: flex;
+    fill: var(--primaryColor);
+    flex-shrink: 0;
+    flex-grow: 0;
+    width: 250px;
+    height: 250px;
+    margin-right: 30px;
+  }
+
+  @media screen and (min-width: ${mediaSizes.smallDesktop}) {
+  }
+`;
+
+export const Wrap = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 10px;
+  width: 100%;
+  font-size: 14px;
+
+  & span {
+    color: var(--primaryColor);
+  }
+
+  @media screen and (min-width: 320px) {
+    font-size: 16px;
+  }
+
+  @media screen and (min-width: ${mediaSizes.mobile}) {
+    font-size: 18px;
+    gap: 15px;
+  }
+
+  @media screen and (min-width: ${mediaSizes.tablet}) {
+    font-size: 22px;
+    gap: 20px;
+  }
+
+  @media screen and (min-width: ${mediaSizes.smallDesktop}) {
+    width: 80%;
+    font-size: 24px;
+  }
+`;
+
+export const Word = styled.h1`
+  display: flex;
+  flex-wrap: wrap;
+  gap: 7px;
+  font-size: 14px;
+  word-break: break-all;
+  white-space: pre-wrap;
+
+  & p {
+    color: var(--primaryColor);
+    &::first-letter {
+      text-transform: uppercase;
+    }
+  }
+
+  @media screen and (min-width: 320px) {
+    font-size: 16px;
+  }
+
+  @media screen and (min-width: ${mediaSizes.mobile}) {
+    font-size: 20px;
+  }
+
+  @media screen and (min-width: ${mediaSizes.tablet}) {
+    font-size: 22px;
+  }
+
+  @media screen and (min-width: ${mediaSizes.smallDesktop}) {
+    font-size: 24px;
+  }
+`;
+
+export const ButtonsContainer = styled.div`
+  display: flex;
+  justify-content: end;
+  gap: 30px;
+  margin-top: 20px;
+
+  @media screen and (min-width: ${mediaSizes.tablet}) {
+    margin-top: 50px;
+  }
+`;
---
+++ components/Modals/LearningWordModal/LearningWordModal.tsx
@@ -0,0 +1,127 @@
+import { FC, useEffect, MouseEvent, useState } from "react";
+import { ModalProps } from "@/types/modals";
+import { WordI } from "@/types/words";
+import { Portal } from "../Portal";
+import { ButtonClose } from "@/components/Buttons";
+import { format, isBefore, parseISO } from "date-fns";
+import { ButtonIcon } from "@/components/Buttons";
+import { useWordsState } from "@/wordsState/wordsState";
+import { ConfirmWordModal } from "..";
+import {
+  BackDrop,
+  Modal,
+  CloseButtonWrap,
+  Vocary,
+  Wrap,
+  Word,
+  ButtonsContainer,
+} from "./LearningWordModal.styled";
+
+export interface AddWordModalProps extends ModalProps {
+  word: WordI | null;
+  lvl: "vocabulary" | "firstLvl" | "secondLvl" | "thirdLvl";
+}
+
+export const LearningWordModal: FC<AddWordModalProps> = ({
+  isOpen,
+  onClose,
+  word,
+  lvl,
+}) => {
+  const { deleteWord } = useWordsState();
+  const [confirmWordModalOpen, setConfirmWordModalOpen] = useState(false);
+
+  const handleBackDropClick = (e: MouseEvent<HTMLDivElement>) => {
+    if (e.target === e.currentTarget) {
+      onClose();
+    }
+  };
+
+  useEffect(() => {
+    if (!isOpen) return;
+
+    const handleEscBtnClick = (e: KeyboardEvent) => {
+      if (e.key === "Escape") {
+        onClose();
+      }
+    };
+
+    document.addEventListener("keydown", handleEscBtnClick);
+
+    return () => {
+      document.removeEventListener("keydown", handleEscBtnClick);
+    };
+  }, [isOpen]);
+
+  useEffect(() => {
+    if (confirmWordModalOpen === true) onClose();
+  }, [confirmWordModalOpen]);
+
+  if (!word) return null;
+
+  const handleDeleteButton = () => {
+    deleteWord({ id: word._id, deleteFrom: lvl });
+    onClose();
+  };
+
+  return (
+    <>
+      <Portal>
+        <BackDrop
+          id="backDrop"
+          onClick={handleBackDropClick}
+          className={isOpen ? "active" : ""}
+        >
+          <Modal>
+            <CloseButtonWrap>
+              <ButtonClose fnc={() => onClose()} />
+            </CloseButtonWrap>
+
+            <Vocary />
+
+            <Wrap>
+              <Word>
+                Word: <p>{word.word}</p>
+              </Word>
+              <p>
+                Added:{" "}
+                <span>
+                  {format(parseISO(word.addedAt as any), "dd.MM HH:mm")}
+                </span>
+              </p>
+              <p>
+                Can be confirmed:{" "}
+                <span>
+                  {isBefore(parseISO(word.canByConfirmed as any), new Date())
+                    ? "Now"
+                    : format(
+                        parseISO(word.canByConfirmed as any),
+                        "dd.MM HH:mm"
+                      )}
+                </span>
+              </p>
+
+              <ButtonsContainer>
+                <ButtonIcon fnc={handleDeleteButton} icon="dumpster" />
+
+                {isBefore(parseISO(word?.canByConfirmed as any), new Date()) ? (
+                  <ButtonIcon
+                    fnc={() => setConfirmWordModalOpen(true)}
+                    icon="rightArrow"
+                  />
+                ) : null}
+              </ButtonsContainer>
+            </Wrap>
+          </Modal>
+        </BackDrop>
+      </Portal>
+
+      <ConfirmWordModal
+        isOpen={confirmWordModalOpen}
+        onClose={() => setConfirmWordModalOpen(false)}
+        word={word}
+        lvl={lvl}
+      />
+    </>
+  );
+};
